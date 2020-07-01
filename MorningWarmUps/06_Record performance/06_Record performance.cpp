734d620c0efab9c3be587c11076decbd503f8e90
Justin <justinkatic4@gmail.com>
2020-07-01 10:10:43 +1000

    added 06 stopwatch to run things

---
+++ MorningWarmUps/06_Record performance/06_Record performance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <chrono>
+
+int DoSomeLongRunningThing()
+{
+	int counter = 0;
+	for (int i = 0; i < 9000; i++)
+	{
+		std::cout << " " << std::endl;
+		counter += 1;
+	}
+	return counter;
+}
+
+int main(int argc, char** argv)
+{
+	auto startTime = std::chrono::steady_clock::now();
+	std::cout << "Hello World" << std::endl;
+	auto endTime = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeDiff = endTime - startTime;
+
+
+	startTime = std::chrono::steady_clock::now();
+	DoSomeLongRunningThing();
+	endTime = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeDiff1 = endTime - startTime;
+
+	std::cout << "first function: "<< timeDiff.count() << std::endl;
+	std::cout << "second function: " << timeDiff1.count() << std::endl;
+
+}
